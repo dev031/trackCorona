3decff8be3dce354a14eeeacfbb0850b2630d581
fz md <80146318+dev031@users.noreply.github.com>
2021-03-06 22:28:36 +0530

    Update head_vw.php

--- application/views/template/head_vw.php
+++ application/views/template/head_vw.php
@@ -7,8 +7,8 @@
     <meta name="keywords" content="Corona, COVID-19, Coronavirus, Coronavirus Tracker">
     <meta name="author" content="fzmd">
     <meta http-equiv="X-UA-CompatibBle" content="IE=edge" />
+    <script data-ad-client="ca-pub-9189306579090915" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <title><?php echo $title;?></title>
     <link rel="shortcut icon" href="<?php echo base_url();?>assets/images/favicon.ico">
-    <script data-ad-client="ca-pub-9189306579090915" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <?php $this->load->view('template/css_vw');?>
     
